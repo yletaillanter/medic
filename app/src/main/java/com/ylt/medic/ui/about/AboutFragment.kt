86dccdab4e8a865ffe163ee3496f7e500c8023b8
yletaillanter <y.letaillanter@gmail.com>
2021-02-26 08:24:39 +0100

    About page

--- app/src/main/java/com/ylt/medic/ui/about/AboutFragment.kt
+++ app/src/main/java/com/ylt/medic/ui/about/AboutFragment.kt
@@ -6,13 +6,21 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ylt.medic.R
-import timber.log.Timber
+import mehdi.sakout.aboutpage.AboutPage
+
 
 class AboutFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val root = inflater.inflate(R.layout.fragment_about, container, false)
-
-        return root
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return AboutPage(context)
+            .isRTL(false)
+            .setDescription(getString(R.string.app_description))
+            .addGroup(getString(R.string.contact_group))
+            .addEmail("ylt.medicament@gmail.com", "Email")
+            .create()
     }
 }
